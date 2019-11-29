1d098a7d2a1e97ff058995e05988fac906bd5546
AGAR <t80ttt@gmail.com>
2019-11-29 20:40:26 +0200

    timer_unfinished

--- secondTask/src/app/app.component.ts
+++ secondTask/src/app/app.component.ts
@@ -8,5 +8,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'secondTask';
 
+  interval: number = -1;
+  isRunning: boolean = false;
+  buttonText: string = "Start";
+
   titleProperty: string = "Title"
+
+  timer(){
+    this.isRunning ? this.start() : this.pause()
+  }
+
+  start(){
+    this.buttonText = "Pause";
+    this.setInterval();
+    this.isRunning = true;
+    while (this.interval > 0 && this.isRunning){
+      this.oneSecondTimeout();
+      this.interval -= 1;
+    }
+  }
+  
+  pause(){
+    this.isRunning = false;
+  }
+
+  private setInterval(){
+    this.interval = 10;
+  }
+
+  clearInterval(){
+    this.interval = -1;
+  }
+
+  oneSecondTimeout() {
+    setTimeout (() => {
+      }, 1000);
+    }
 }
+
--- secondTask/src/app/message/message.component.ts
+++ secondTask/src/app/message/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-message',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+
+  @Input() interval: number
 
   constructor() { }
 
